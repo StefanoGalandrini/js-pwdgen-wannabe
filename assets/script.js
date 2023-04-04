d5756f017685ba6708d3ba72f162d5c70f162e34
StefanoGalandrini <stefe68@gmail.com>
2023-04-04 14:41:56 +0200

    Add javascript pseudocode

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+/*
+	Chiedere all'utente il nome e inserirlo nella variabile firstName
+	Chiedere all'utente il cognome e inserirlo nella variabile lastName
+	Chiedere all'utente il colore preferito e inserilo nella variabile prefColor
+	Generare la password concatenando le variabili firstName, lastName e prefColor nella variabile PassWord
+	Selezionare l'ID dello span che dovrà contenere la variabile PassWord
+	Mostrare nella pagina un messaggio con lo span che comunica il contenuto della variabile PassWord
+*/
+
